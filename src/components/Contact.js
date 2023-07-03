da5b434ae3b02ca6db365506be82dbcb54720dd9
Sean <seanhoyeonjin@gmail.com>
2023-07-03 11:21:03 -0400

    played around with adding a wavey background using svg

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="contact-container" id="contact">
-      <h2 className="contact-header">Contact</h2>
+      <h2 className="contact-header">Contact Me</h2>
       <p>
         Feel free to contact me by submitting the form below and I will get back
         to you as soon as possible!
@@ -23,8 +23,7 @@ function Contact() {
           <label>Message</label>
           <textarea type="text" placeholder="Enter Your Message" />
         </div>
-
-        <input type="submit" />
+        <button type="submit"> Submit</button>
       </form>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,10 +3,20 @@ import "./Projects.css";
 
 function Projects() {
   return (
-    <div className="projects-container" id="projects">
-      <h2 className="projects-header">List of Projects</h2>
-      <p>These are my projects</p>
-    </div>
+    <>
+      <div className="projects-container" id="projects">
+        <h2 className="projects-header">List of Projects</h2>
+        <p>These are my projects</p>
+      </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#eae7dc"
+          fill-opacity="1"
+          d="M0,192L48,202.7C96,213,192,235,288,213.3C384,192,480,128,576,117.3C672,107,768,149,864,144C960,139,1056,85,1152,58.7C1248,32,1344,32,1392,32L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+        ></path>
+      </svg>
+    </>
   );
 }
 
