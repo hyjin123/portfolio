f13bc66896d5395f7d6f1cd70d0a0278b8a359d7
Sean <seanhoyeonjin@gmail.com>
2023-07-10 11:48:34 -0400

    using framer motion and react intersection observer to animate elements, got the basics working

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,37 @@
-import React from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 import "./Projects.css";
 
 function Projects() {
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  const boxVariant = {
+    visible: { opacity: 1, scale: 1 },
+    hidden: { opacity: 0, scale: 0 },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
   return (
     <>
       <div className="projects-container" id="projects">
-        <img
+        <motion.img
+          ref={ref}
           className="project-img"
           src={require("./flower2.png")}
           alt="sean-bitmoji"
           max-width="30%"
+          animate={control}
+          initial="hidden"
+          variants={boxVariant}
         />
         <h2 className="projects-header">List of Projects</h2>
         <p>These are my projects</p>
